1d1a4c2dd568e55dc793c41dafef171dc9cb722e
Engin Durna <eng.durna@gmail.com>
2022-08-17 18:12:41 +0200

    cc

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -2,5 +2,6 @@ public class GitIgnore {
     public static void main(String[] args) {
         System.out.println("gitignore deneme 1");
         System.out.println("gitignore deneme 2");
+        System.out.println("gitignore deneme 3");
     }
 }
