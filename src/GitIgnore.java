3700916da90b59421e65faaa86705d6703f7b232
Engin Durna <eng.durna@gmail.com>
2022-08-17 18:10:46 +0200

    brancconf

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -1,5 +1,6 @@
 public class GitIgnore {
     public static void main(String[] args) {
         System.out.println("gitignore deneme 1");
+        System.out.println("gitignore deneme 2");
     }
 }
